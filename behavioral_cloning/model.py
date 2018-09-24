5588450c713967b090059b00d7602ec9ef5f051d
Viktor Kuznietsov <victor.kuzn@gmail.com>
2018-09-24 17:48:12 -0400

    added model and its weights for behavior cloning.

---
+++ behavioral_cloning/model.py
@@ -0,0 +1,120 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Conv2D, BatchNormalization, MaxPooling2D, Lambda, Dropout, Flatten, Cropping2D
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+import cv2
+import csv
+
+samples = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
+
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+import cv2
+import numpy as np
+import sklearn
+
+data_folder_name = './data/IMG/'
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            
+            for batch_sample in batch_samples:
+                center_name = data_folder_name + batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(center_name)
+                center_angle = float(batch_sample[3])
+                
+                correction = 0.8 # this is a parameter to tune
+                left_angle = center_angle + correction
+                right_angle = center_angle - correction
+                
+                left_name = data_folder_name + batch_sample[1].split('/')[-1]
+                left_image = cv2.imread(left_name)
+                
+                right_name = data_folder_name + batch_sample[2].split('/')[-1]
+                right_image = cv2.imread(right_name)
+                
+                images.extend([center_image, left_image, right_image])
+                angles.extend([center_angle, left_angle, right_angle])
+
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+            
+def build_model():
+    model = Sequential()
+    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+    model.add(Lambda(lambda x: (x/127.5)-1.))
+    model.add(Conv2D(24, kernel_size=(5,5), strides=(1,1), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(36, kernel_size=(5,5), strides=(1,1), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(48, kernel_size=(5,5), strides=(1,1), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Conv2D(64, kernel_size=(3,3), strides=(1,1), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Flatten())
+    model.add(Dense(1164))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.25))
+    model.add(Activation('relu'))
+    model.add(Dense(100))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.25))
+    model.add(Activation('relu'))
+    model.add(Dense(50))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.25))
+    model.add(Activation('relu'))
+    model.add(Dense(1))
+    
+    model.compile(optimizer='adam', loss='mse')
+    
+    return model
+    
+def train_model(model, train_generator, validation_generator, epochs=3):
+    early_stopping_callback = EarlyStopping(monitor='val_loss', patience=2)
+    checkpoint_callback = ModelCheckpoint('best_model.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+    
+    model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator,             nb_val_samples=len(validation_samples), nb_epoch=epochs, callbacks=[early_stopping_callback, checkpoint_callback])
+
+
+    
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+model = build_model()
+
+train_model(model, train_generator, validation_generator, 2)
+
+model.save('model.h5')
+
+
+
+
+
+              
